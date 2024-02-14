b6ff3d3ef566844da0e920d0ea02ee93a0aa09f9
Wink Saville <wink@saville.com>
2024-02-14 14:57:53 -0800

    feat: Bump version to 0.3.0
    Upgrade dependencies and add some log::debug!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use custom_logger::env_logger_init;
 
 fn main() {
-    env_logger_init("info");
+    env_logger_init("warn");
+    log::debug!("enter");
 
     println!("println output");
     log::info!("hello");
+
+    log::debug!("exit");
 }
